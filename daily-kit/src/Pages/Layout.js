7fc156eb1747ed6d07ebd39324afb89feba2e450
heejinlee07 <heejinlee07@gmail.com>
2020-05-25 18:58:57 +0900

    todo page completed

--- daily-kit/src/Pages/Layout.js
+++ daily-kit/src/Pages/Layout.js
@@ -1,33 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Route, NavLink } from 'react-router-dom';
-=======
 import { NavLink } from 'react-router-dom';
 import MainRouter from '../Router/MainRouter';
 import '../Css/main.css';
->>>>>>> e8c8da4e92ac4e07d952bb41219c107212c6f9cf
 
 const Layout = () => {
   const activeStyle = {
     backgroundColor: '#599681',
     fontWeight: 800,
-<<<<<<< HEAD
-  };
-
-  return (
-    <div className={'outerWrapper'}>
-      <nav>
-        <h1>Date</h1>
-        <h2>Time</h2>
-        <ul>
-          <li>
-            <NavLink exact to="/" activeStyle={activeStyle} />
-          </li>
-          <li>Weather</li>
-          <li>News</li>
-          <li>About</li>
-        </ul>
-=======
     width: '100%',
   };
 
@@ -64,7 +43,6 @@ const Layout = () => {
           </li>
         </ul>
         <MainRouter />
->>>>>>> e8c8da4e92ac4e07d952bb41219c107212c6f9cf
       </nav>
     </div>
   );
--- daily-kit/src/Pages/Todos.js
+++ daily-kit/src/Pages/Todos.js
@@ -1,34 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TodosApi from '../Components/TodosApi';
+import { api } from '../Components/TodosApi';
 import Todolist from '../Components/Todolist';
+import '../Css/todos.css';
 
 const Todos = () => {
   const [isLoading, setLoading] = useState(false);
   const [hasError, setError] = useState(false);
   const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     const getTodoList = async () => {
       try {
         setLoading(true);
-        const { data } = await axios.get('/todos');
+        const { data } = await api.get('/todos');
         console.log(data);
         setTodos(data);
       } catch (e) {
         setError(true);
         console.log(e.message);
       }
-      getTodoList();
+      setLoading(false);
     };
+    getTodoList();
   }, []);
+
+  const addTodo = async () => {
+    await api.post('/todos', {
+      content: value,
+      status: 'incompleted',
+    });
+    const { data } = await api.get('/todos');
+    setTodos(data);
+    console.log(data);
+    setValue('');
+  };
+
+  const deleteTodo = async (id) => {
+    await api.delete(`/todos/${id}`);
+    const { data } = await api.get('/todos');
+    setTodos(data);
+  };
+
+  const changeStatus = async (todo) => {
+    await api.put(`/todos/${todo.id}`, {
+      ...todo,
+      status: todo.status === 'incompleted' ? 'completed' : 'incompleted',
+    });
+    const { data } = await api.get('/todos');
+    console.log(data);
+    setTodos(data);
+  };
+
+  const editContent = async (todo, content) => {
+    await api.put(`/todos/${todo.id}`, {
+      ...todo,
+      content,
+    });
+    const { data } = await api.get('/todos');
+    console.log(data);
+    setTodos(data);
+  };
+
+  const onKeypress = (e) => {
+    if (e.key === 'Enter') console.log(e.target.value);
+    addTodo();
+  };
 
   return (
     <div className="todoContainer">
-      <h1>Today's Todo</h1>
-      <input placeholder="Add todos"></input>
+      {isLoading && <h1 className="message">Now Loading...</h1>}
+      {hasError && <h1 className="message">has Error!</h1>}
+      <div>
+        <h1 className="headerStyle">Today's Todo</h1>
+      </div>
+      <div className="containerStyle">
+        <input
+          value={value}
+          className="todoInputStyle"
+          placeholder="Add todos"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          onKeyPress={onKeypress}
+        ></input>
+        <button className="buttonStyle" onClick={addTodo}>
+          add
+        </button>
+      </div>
+      <div className="countSection">
+        <div>Total: {todos.length}</div>
+        <div>
+          Completed Todos:{' '}
+          {todos.filter((todo) => todo.status === 'completed').length}
+        </div>
+        <div>
+          Incompleted Todos:{' '}
+          {todos.filter((todo) => todo.status === 'incompleted').length}
+        </div>
+      </div>
       {todos.map((todo) => (
-        <Todolist todo={todo} />
+        <Todolist
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          changeStatus={changeStatus}
+          editContent={editContent}
+        />
       ))}
     </div>
   );
